d9a1f6dd2e41eafb7fd1f30ac093ddeb5f13f879
Jens Becker <info@jhb.software>
2024-12-16 15:47:25 +0100

    fix(web): allow loading drafts in preview mode

--- web/src/cms/getPageData.ts
+++ web/src/cms/getPageData.ts
@@ -1,7 +1,7 @@
 import { CMS_URL } from 'astro:env/server'
-import type { SeoMetadata } from 'cms/src/payload-types'
-import type { StaticPagePropsFrontend } from './getStaticPaths'
 import type { PageProps } from 'cms/src/endpoints/pageProps'
+import type { SeoMetadata } from 'cms/src/payload-types'
+import type { Locale } from './types'
 
 export type PageMetaData = {
   meta: SeoMetadata
@@ -12,8 +12,28 @@ export type PageData = PageMetaData & {
 }
 
 // TODO: use the payload SDK to get the page data
-export async function getPageData(props: PageProps): Promise<PageData> {
-  const response = await fetch(`${CMS_URL}/api/${props.collection}/${props.id}`)
+export async function getPageData(
+  props: PageProps,
+  locale: Locale,
+  options?: { preview?: boolean },
+): Promise<PageData> {
+  const queryOptions = new URLSearchParams()
+
+  queryOptions.set('locale', locale)
+  queryOptions.set('limit', '1')
+  queryOptions.set('pagination', 'false')
+
+  if (options?.preview) {
+    queryOptions.set('draft', 'true')
+    queryOptions.set('where[_status][in]', 'published,draft')
+  } else {
+    queryOptions.set('draft', 'false')
+    queryOptions.set('where[_status][equals]', 'published')
+  }
+
+  const response = await fetch(
+    `${CMS_URL}/api/${props.collection}/${props.id}?${queryOptions.toString()}`,
+  )
   const data = await response.json()
 
   if (!response.ok) {
