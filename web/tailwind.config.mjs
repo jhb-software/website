76fd2fd0949113600b2d960450b444960a8d3e8e
Jens Becker <info@jhb.software>
2024-12-17 09:59:48 +0100

    ui(web): add colors and font

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -1,8 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#1e3a8a',
+        primaryDark: '#162D6E',
+        'neutral-150': '#EDEDED',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#333',
+          },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
