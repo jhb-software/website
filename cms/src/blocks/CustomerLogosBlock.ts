360bb3ca29dad68a2c9affd083bc95f495eb37e4
Jens Becker <info@jhb.software>
2024-12-17 18:31:53 +0100

    feat(web): add customer logo block component

--- cms/src/blocks/CustomerLogosBlock.ts
+++ cms/src/blocks/CustomerLogosBlock.ts
@@ -15,33 +15,12 @@ export const CustomerLogosBlock: Block = {
   },
   fields: [
     {
-      // This virtual field makes the data directly available to the frontend when a document with the block is requested
       name: 'customers',
       type: 'relationship',
       relationTo: 'customers',
       hasMany: true,
       required: true,
-      virtual: true,
-      // As the value of the field is set by the hook, do not validate it
-      validate: () => true,
-      admin: {
-        readOnly: true,
-      },
-      hooks: {
-        afterRead: [
-          async ({ req: { payload } }) => {
-            const customers = await payload.find({
-              collection: 'customers',
-              limit: 100,
-              select: {
-                // only select the id
-              },
-            })
-
-            return customers.docs.map((customer) => customer.id)
-          },
-        ],
-      },
+      maxRows: 12,
     },
   ],
 }
