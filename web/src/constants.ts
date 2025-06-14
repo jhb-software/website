2bb98b4b2045a93c86c136a2a4505f05d38d2448
Jens Becker <info@jhb.software>
2025-06-14 13:41:07 +0200

    feat(web): add og image

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,1 +1,14 @@
+import { SITE_URL } from 'astro:env/client'
+import type { Locale } from './cms/types'
+
+export const logoPath = '/logo_medium.png'
+export const logoURL = new URL(logoPath, SITE_URL).toString()
+
+export const ogImagePath = (locale: Locale) => {
+  return `/og_${locale}.png`
+}
+export const ogImageURL = (locale: Locale) => {
+  return new URL(ogImagePath(locale), SITE_URL).toString()
+}
+
 export const siteName = 'JHB Software'
