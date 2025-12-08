01f917479d3dc8047084a2f25f3a409e5f4003b7
Jens Becker <info@jhb.software>
2025-12-08 21:31:38 +0100

    style: apply prettier formatting

--- web/src/cms/sdk/index.ts
+++ web/src/cms/sdk/index.ts
@@ -5,12 +5,12 @@ import { createCachedFetch } from './cachedFetch'
 
 /**
  * Payload SDK instance with caching support.
- * 
+ *
  * This cache will cache responses forever, which is acceptable because the SDK-level cache
  * is only used during static site building (SSG), not during preview or SSR requests.
- * This means cached data will be "baked into" the generated static site and won't impact 
+ * This means cached data will be "baked into" the generated static site and won't impact
  * local development or preview/SSR modes, which always request fresh data.
- * 
+ *
  * Caching is opt-in and controlled via RequestInit headers:
  * - Set `X-Use-Cache: 'true'` to enable caching (only for GET requests).
  * - Omit or set `X-Use-Cache: 'false'` to disable caching (default).
